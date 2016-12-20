b549375cb4a4614378f369b76ee0301baa7d9dba
sergy litkovskiy <slitkovskiy@grossum.com>
2016-12-20 22:14:37 +0200

    angular2 implementation v11

--- application/views/admin/blocks/menu_admin.php
+++ application/views/admin/blocks/menu_admin.php
@@ -9,6 +9,7 @@
     <li><a id='cat_name' href='/backend/landing_articles'>Closed trening sistem</a></li>
     <li><a id='cat_name' href='/backend/spec_mailer_statistics'>Спец.  рассылка</a></li>
     <li><a id='cat_name' href='/backend/sale_page_list'>Sale page</a></li>
+    <li><a id='cat_name' href='/backend/sale_categories'>Категории продуктов</a></li>
     <li><a id='cat_name' href='/backend/sale_products_list'>Sale products</a></li>
     <li><a id='cat_name' href='/backend/sale_products_statistic'>Sale statistic</a></li>
     <li><a id='cat_name' href='/backend/announce_list'>Анонсы</a></li>
